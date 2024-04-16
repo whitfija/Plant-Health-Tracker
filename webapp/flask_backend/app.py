30f88e66623b72ddcf5d6e8f7a597bde9d34a3e3
apek3 <apek3@gatech.edu>
2024-04-16 18:14:32 -0400

    Flask backend, missed it in last commit

---
+++ webapp/flask_backend/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+app = Flask(__name__, static_url_path="")
+
+def scrape_news():
+    url = "https://www.google.com"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    headlines = []
+    for headline in soup.find_all("a"):
+        headlines.append(headline.text)
+    return headlines
+
+def scrape_plant(ip):
+    url = ip
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        sensorReadings = []
+        for sensorReading in soup.find_all("h1"):
+            sensorReadings.append(sensorReading.text)
+        return sensorReadings
+    except Exception as e:
+        return []
+
+
+@app.route("/scrapeData")
+def scrapeData():
+    headlines = scrape_news()
+    #return render_template("index.html")
+    return {'Data': headlines}
+
+@app.route("/scrapePlantData/<string:ip>")
+def scrapePlantData(ip):
+    latest_data = scrape_plant(ip)
+    return {'Data': latest_data}
+
+if __name__ == "__main__":
+    app.run(debug=True)
